9e2b5a5eda6a781a1728f705db6c94e9ba520e71
IXIXIXIXIXIXIXIXIX <ix.scott.anderson@gmail.com>
2021-02-16 15:34:41 +0000

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,68 @@
-import logo from './logo.svg';
+import {useState, useEffect} from 'react';
 import './App.css';
 
 function App() {
+
+  const [inputNumber, setInputNumber] = useState(1);
+  const [answer, setAnswer] = useState(1);
+  const [guess, setGuess] = useState("");
+  const [correct, setCorrect] = useState("");
+  const [time, setTime] = useState(0);
+
+  setInterval(() => {
+      setTime(time + 1);
+
+    }, 1000);
+
+  useEffect(() => {
+
+     console.log("Side effect triggered"); 
+
+    if (inputNumber % 3 === 0 && inputNumber % 5 === 0) {
+      setAnswer("fizzbuzz")
+    } else if (inputNumber % 3 === 0) {
+      setAnswer("fizz")
+    } else if (inputNumber % 5 === 0) {
+      setAnswer("buzz")
+    } else {
+      setAnswer(inputNumber);
+    }
+
+    setTime(0);
+    
+    setCorrect("");
+  }, [inputNumber]);
+
+  const handlePlay = () => {
+    setInputNumber(inputNumber + 1);
+
+  };
+
+
+const handleGuess = ((event) => {
+  
+  event.preventDefault();
+  if (guess.toLowerCase() == answer) {
+    setCorrect("you got it right")
+  } else {
+    setCorrect("you got it wrong, shan times!")
+  }
+});
+
+const handleInputGuess = (event) => {
+  setGuess(event.target.value);
+};
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>{time}</h1>
+      <h2>Current number: {inputNumber}</h2>
+      <button value={inputNumber} onClick={handlePlay}>Next Number</button>
+      <form onSubmit={handleGuess}>
+      <input type="text" placeholder="Your guess: fizz, buzz, fizzbuzz or number" value={guess} onChange={handleInputGuess}/>
+      <input type="submit" value="Guess" />
+      </form>
+      <h1>{correct}</h1>
     </div>
   );
 }
